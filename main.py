c37a6b2e97829dbe74d4332bcd4d9013e3e368b4
ATmax <atmax1425@gmail.com>
2022-06-08 15:42:07 +0530

    1st task

---
+++ main.py
@@ -0,0 +1,12 @@
+# 1st Task
+# Create a small data structure that holds details
+
+IPL_DATA = {
+    # Name of the teams
+    "Team": ["GT", "LSG", "RR", "DC", "RCB", "KKR", "PBKS", "SRH", "CSK", "MI"],
+    # points they have earned
+    "Pts": [20, 18, 16, 14, 14, 12, 12, 12, 8, 6],
+    #  result of last 5 matches
+    "Last_5": [[1, 1, 0, 0, 1], [1, 0, 0, 1, 1], [1, 0, 1, 0, 0], [1, 1, 0, 1, 0], [0, 1, 1, 0, 0],
+               [0, 1, 1, 0, 1], [0, 1, 0, 1, 0], [1, 0, 0, 0, 0], [0, 0, 1, 0, 1], [0, 1, 0, 1, 1]]
+}
